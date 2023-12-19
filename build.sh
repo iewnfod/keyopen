2eb555972090c9808744b4a2c5f2e4a3d4f326e2
Iewnfod <shangzhiqiuyue@outlook.com>
2023-12-19 11:09:44 +0800

    Add release profile and x86 darwin support

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
-cargo tauri build
+echo "Building for aarch64 darwin"
+cargo tauri build --target aarch64-apple-darwin
+echo "Building for x86_64 darwin"
+cargo tauri build --target x86_64-apple-darwin
