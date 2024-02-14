36ec816f2d24ef6639d96c57c397c5da32cb6e1f
Iewnfod <shangzhiqiuyue@outlook.com>
2024-02-14 12:18:29 +0800

    hide title bar

--- src/js/systems.js
+++ src/js/systems.js
@@ -1,10 +1,10 @@
-import { invoke } from "./lib/api/tauri.js";
+import { invoke } from "../lib/api/tauri.js";
 
 let system = await invoke("get_system");
 
 console.log(system);
 
-if (system != "macos") {
+if (system !== "macos") {
 	// 禁用开机自启动选项
 	let startup = document.getElementById('startup');
 	startup.setAttribute('disabled', 'disabled');
