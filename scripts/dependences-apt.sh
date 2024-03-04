63fc05da57b60807acad9c160c4cb51f960e5d22
Iewnfod <shangzhiqiuyue@outlook.com>
2024-03-04 15:52:43 +0800

    chore: add apt dependences install script

---
+++ scripts/dependences-apt.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y \
+	pkg-config \
+	libglib2.0-dev \
+	libgdk-pixbuf2.0-dev \
+	libgtk-3-dev \
+	libsoup2.4-dev \
+	libjavascriptcoregtk-4.0-dev \
+	libwebkit2gtk-4.0-dev \
+	libappindicator3-dev
