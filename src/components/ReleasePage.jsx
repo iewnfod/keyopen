f73fe2b776c95d99617cd4e20df89447721bc25a
Iewnfod <shangzhiqiuyue@outlook.com>
2024-06-10 09:59:58 +0800

    feat: release page to center

--- src/components/ReleasePage.jsx
+++ src/components/ReleasePage.jsx
@@ -1,4 +1,4 @@
-import {Box, Button, Card, CardContent, CardMedia, IconButton, Link, Paper} from "@mui/material";
+import {Box, Button, Card, CardContent, CardMedia, Grid, IconButton, Link, Paper} from "@mui/material";
 import Typography from "@mui/material/Typography";
 import packageInfo from "../../package.json";
 import logo from "../../src-tauri/icons/icon.svg";
@@ -18,34 +18,44 @@ export default function ReleasePage() {
     }
 
     return (
-        <Box sx={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-            <Box sx={{
-                mt: 5,
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-                p: 2,
-                gap: 1
-            }}>
-                <img src={logo} alt="" width="200px"/>
+        <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            sx={{position: 'absolute'}}
+            style={{ minHeight: "99vh", left: 0, top: 0, width: '100vw' }}
+        >
+            <Grid xs={5}>
                 <Box sx={{
+                    mt: 5,
                     display: "flex",
-                    flexDirection: "row",
-                    alignItems: "flex-end",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    p: 2,
                     gap: 1
                 }}>
-                    <Typography component="div" variant="h5">
-                        Key Open
-                    </Typography>
-                    <Typography component="div" variant="body1">
-                        v{packageInfo.version}
-                    </Typography>
+                    <img src={logo} alt="" width="200px"/>
+                    <Box sx={{
+                        display: "flex",
+                        flexDirection: "row",
+                        alignItems: "flex-end",
+                        gap: 1
+                    }}>
+                        <Typography component="div" variant="h5">
+                            Key Open
+                        </Typography>
+                        <Typography component="div" variant="body1">
+                            v{packageInfo.version}
+                        </Typography>
+                    </Box>
+                    <Button onClick={openGithub}>
+                        View More on Github
+                    </Button>
                 </Box>
-                <Button onClick={openGithub}>
-                    View More on Github
-                </Button>
-            </Box>
-        </Box>
+            </Grid>
+        </Grid>
     );
 }
